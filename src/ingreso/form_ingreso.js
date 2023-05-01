dcd96a48261c9f303be64ff8de2751e25d649b68
LucasA75 <lucasacevedocastro@gmail.com>
2023-05-01 08:44:32 -0400

    Check token , indicador de auth incorrecto

--- src/ingreso/form_ingreso.js
+++ src/ingreso/form_ingreso.js
@@ -1,13 +1,23 @@
 
 const form = document.querySelector("form");
 
-form.addEventListener("submit", async (e)=>{
+const titulo = document.querySelector("h2")
+const alerta = document.createElement("div")
+const salto = document.createElement("br")
+const text = document.createTextNode("El Usuario y/o la contraseña son erroneos.")
+const text2 = document.createTextNode("Intentelo nuevamente")
+alerta.appendChild(text)
+alerta.appendChild(salto)
+alerta.appendChild(text2)
+alerta.setAttribute("class", "div_error")
+
+form.addEventListener("submit", async (e) => {
   e.preventDefault()
   const formElement = e.currentTarget;
   const formData = new FormData(formElement)
   const email = formData.get("email")
   const password = formData.get("password")
-  
+
   const User = {
     email,
     password
@@ -19,24 +29,25 @@ form.addEventListener("submit", async (e)=>{
   const url = baseUrl + "/login";
   const fetchConfig = {
     method: 'POST',
-    Headers:{
-      'Content-Type':'application/json'
+    Headers: {
+      'Content-Type': 'application/json'
     },
-    body:JSON.stringify(User)
+    body: JSON.stringify(User)
   }
 
   try {
-    const respuesta     = await fetch(url, fetchConfig);
+    const respuesta = await fetch(url, fetchConfig);
     // si la respuesta no es OK 
-    if( !respuesta.ok ) {
-       // gestionar error o mensajes recibidos 
-       console.error("La respuesta no está OK");
-       return;
-    } 
+    if (!respuesta.ok) {
+      // gestionar error o mensajes recibidos 
+      console.error("La respuesta no está OK");
+      titulo.appendChild(alerta)
+      return;
+    }
 
     // en caso de exito 
-    const objetoJson    = await respuesta.json(); 
-    console.dir( objetoJson);
+    const objetoJson = await respuesta.json();
+    console.dir(objetoJson);
 
     const usuario = objetoJson.usuario;
     // caso éxito 
@@ -44,20 +55,17 @@ form.addEventListener("submit", async (e)=>{
     // guardo datos del usuario y TOKEN 
     localStorage.setItem('usuario', JSON.stringify(usuario));
     // redirijo a página protegida 
-    window.location = '../dashboard.html';
-
-} catch (error) {
+    window.location = '../../admin/categoria/index.html';
+    /*window.location = '../dashboard.html'; */
+  } catch (error) {
     // gestion errores 
-    console.error( error.code );
-    console.error( error.message );
-}
+    console.error(error.code);
+    console.error(error.message);
+  }
 
 })
 
 
-/* Conexion de form ingreso a backEnd */
 
 
 
-  
- 
